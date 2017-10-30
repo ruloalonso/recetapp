1029c57977e1b7f5e408f3aca733858553fd11c1
ruloalonso <r.alonso.lopez@gmail.com>
2017-10-30 15:02:16 +0100

    Shopping list y Auth redux (con errores)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AuthModule } from './auth/auth-module';
+import { AuthEffects } from './auth/store/auth.effects';
+import { reducers } from './store/app.reducers';
+
 
 
 @NgModule({
@@ -17,12 +24,16 @@ import { AuthModule } from './auth/auth-module';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
+    HttpClientModule,
     AppRoutingModule,
-    AuthModule,
     SharedModule,
     ShoppingListModule,
-    CoreModule
+    AuthModule,
+    CoreModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([AuthEffects]),
+    StoreRouterConnectingModule,
+    !environment.production ? StoreDevtoolsModule.instrument() : []
   ],
   bootstrap: [AppComponent]
 })
